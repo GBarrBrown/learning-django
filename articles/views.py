b3892c1b8a1a016510f28df22cef1f8e6353acd7
gbarrbrown <gbb7899@gmail.com>
2019-01-23 23:29:40 +1300

    template tags in article_list

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
 def article_list(request):
-    return render(request, 'articles/article_list.html')
+    articles = Article.objects.all().order_by('date')
+    #order_by lets you order items by a field of the table eg date
+    return render(request, 'articles/article_list.html', {'articles':articles})
